1140a4ceddbbf793007eb81b5a32da80845e0cea
mindtrix <mindtrix@ya.ru>
2017-04-23 14:44:50 +0300

    addTypeTranslate

--- js/map.js
+++ js/map.js
@@ -1,46 +1,46 @@
 'use strict';
 var body = document.body;
-var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде']
-var typeOfHouse = [ 'flat', 'house', 'bungalo'];
+var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var typeOfHouse = ['flat', 'house', 'bungalo'];
 var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var checkTime = ['12:00', '13.00', '14.00']; 
+var checkTime = ['12:00', '13.00', '14.00'];
 var dialogPanelTemplate = document.getElementById('lodge-template').content;
 var originalDialogPanel = body.querySelector('.dialog__panel');
 var offerDialog = originalDialogPanel.parentNode;
 var dialogTitle = body.querySelector('.dialog__title');
 var avatarImg = dialogTitle.querySelector('img');
-var random = function(min,max) {
-    return Math.round(Math.random()*(max-min) + (min))
+var random = function (min, max) {
+  return Math.round(Math.random() * (max - min) + (min));
 };
 var adverts = [];
-var genElement = function(n) { 
-  var x = random(300,900);
-  var y =  random(100,500);
-  var randomFeaturesLength = function() {
-  var featuresArrcopy = featuresArr.slice();
-  var counter = random(0, featuresArrcopy.length);
-  for (; counter>0; counter--) {
-    featuresArrcopy.splice(random(0,featuresArrcopy.length-1),1)
+var genElement = function (n) {
+  var x = random(300, 900);
+  var y = random(100, 500);
+  var randomFeaturesLength = function () {
+    var featuresArrcopy = featuresArr.slice ();
+    var counter = random(0, featuresArrcopy.length);
+    for (; counter > 0; counter--) {
+    featuresArrcopy.splice(random(0, featuresArrcopy.length - 1), 1);
   }
-  return featuresArrcopy;
-  }
-  var advertElement =  {
+    return featuresArrcopy;
+  };
+  var advertElement = {
     'author': {
       'avatar': 'img/avatars/user0' + n + '.png'
-     },
+    },
     'offer': {
-      'title': titles[random(0,titles.length-1)], 
+      'title': titles[random(0, titles.length-1)],
       'address': x + ',' + y,
-      'price': random(1000,1000000), 
+      'price': random(1000, 1000000),
       'type': {
-          'flat':"Квартира", 
-          'house':'Дом',
-          'bungalo':'Бунгало'
-        },
-      'rooms': random(1,5),
-      'guests': random(1,10),
-      'checkin': checkTime[random(0,checkTime.length-1)],
-      'checkout': checkTime[random(0,checkTime.length-1)],
+        'flat':'Квартира', 
+        'house':'Дом',
+        'bungalo':'Бунгало'
+      },
+      'rooms': random(1, 5),
+      'guests': random(1, 10),
+      'checkin': checkTime[random(0, checkTime.length - 1)],
+      'checkout': checkTime[random(0, checkTime.length - 1)],
       'features': randomFeaturesLength(),
       'description': '',
       'photos': []
@@ -49,10 +49,10 @@ var genElement = function(n) {
       'x': x,
       'y': y
     }
-  }
+  };
   return advertElement;
-}   
-for (var i = 1; i < 9 ; i++) {
+};
+for (var i = 1; i < 9; i++) {
   adverts.push(genElement(i));
 }
 var map = document.body.querySelector('.tokyo__pin-map');
